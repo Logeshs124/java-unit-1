25026b1bd23d6fb985455f40845715f259da897a
Logesh <logeshsakthi2005@gmail.com>
2025-11-27 14:18:45 +0530

    Add files via upload

---
+++ java_files/MaxOfTwo.java
@@ -0,0 +1,7 @@
+class MaxOfTwo {
+    public static void main(String[] args) {
+        int a = 12, b = 25;
+        int max = (a > b) ? a : b;
+        System.out.println("Maximum = " + max);
+    }
+}
