7f21df4c9b32edb6ddf1322fcc38814b7d7bc5df
Logesh <logeshsakthi2005@gmail.com>
2025-11-27 13:35:24 +0530

    Add files via upload

---
+++ java_files/ArithmeticDemo.java
@@ -0,0 +1,10 @@
+class ArithmeticDemo {
+    public static void main(String[] args) {
+        int a = 15, b = 4;
+        System.out.println("a + b = " + (a + b));
+        System.out.println("a - b = " + (a - b));
+        System.out.println("a * b = " + (a * b));
+        System.out.println("a / b = " + (a / b));
+        System.out.println("a % b = " + (a % b));
+    }
+}
